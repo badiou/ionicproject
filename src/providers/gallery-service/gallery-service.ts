dc1d961efb652b08d80b751c1cc83b3fafdc6f33
Badiou OURO-BANG'NA <badiou.ouro@ceet.tg>
2020-09-03 15:46:47 +0000

    Change API key for pixabay

--- src/providers/gallery-service/gallery-service.ts
+++ src/providers/gallery-service/gallery-service.ts
@@ -11,13 +11,13 @@ import { Http } from '@angular/http';
 export class GalleryServiceProvider {
   //site est notre host. c'est le site hote que lequel se trouve notre api
   //page est le nombre d'élements à afficher par page
-   site:string="https://pixabay.com/api";
+   host:string="https://pixabay.com/api";
   
   constructor(public http: Http) {
     console.log('Hello GalleryServiceProvider Provider');
   }
   chercher(query:string,size:number, page:number){
-   return(this.http.get(this.site+"/?key=13294167-0bea996738bd65a1d40edb810&q="+query+"&per_page="+size+"$page="+page)
+   return(this.http.get(this.host+"/?key=9506396-305a526e54f44f37a702843c5&q="+query+"&per_page="+size+"$page="+page)
       .map(resp=>resp.json()));    
   }
 }
